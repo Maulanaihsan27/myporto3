72e9b376efd2f8ee695741fd967fc83877c260e0
Maulanaihsan27 <muhammad.maulana@students.utdi.ac.id>
2025-06-11 14:19:30 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", async event => {
+  const cache = await caches.open("pwa-assets");
+  // it stores all resources on first SW install
+  cache.addAll([
+    "/",
+    "app.js",
+    "A.jpg",
+    "detail.html",
+    "index.html",
+    "Project One.png"]); 
+});
+ 
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request).then(cachedResponse => {
+	   // It can update the cache to serve updated content on the next request
+         return cachedResponse || fetch(event.request);
+     }
+   )
+  )
+});
