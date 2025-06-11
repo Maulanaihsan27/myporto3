42add26a5ebf1ea63f873880819dc6632fcd7221
Maulanaihsan27 <muhammad.maulana@students.utdi.ac.id>
2025-06-11 14:28:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
-self.addEventListener("install", async event => {
-  const cache = await caches.open("pwa-assets");
-  // it stores all resources on first SW install
-  cache.addAll([
+self.addEventListener("install", event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open("pwa-assets");
+    try {
+      await cache.addAll([
     "/",
     "app.js",
     "A.jpg",
     "detail.html",
     "index.html",
     "Project One.png"]); 
+} catch (err) {
+      console.error("Gagal menyimpan cache:", err);
+    }
+  })());
 });
- 
+
 self.addEventListener("fetch", event => {
-   event.respondWith(
-     caches.match(event.request).then(cachedResponse => {
-	   // It can update the cache to serve updated content on the next request
-         return cachedResponse || fetch(event.request);
-     }
-   )
-  )
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
